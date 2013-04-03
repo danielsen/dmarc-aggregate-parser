c1a1e3091b8e9f2cb5eaa4b2b5d842783b6c3b3d
danielsen <dnielsen@reachmail.com>
2013-04-03 18:36:15 -0500

    Fixed error in setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
 	name="dmarclib",
 	version="0.7",
 	author="Dan Nielsen",
-	author_email="dnielsen@reachmail.com"
+	author_email="dnielsen@reachmail.com",
 	url="https://github.com/danielsen/dmarclib.git",
 	packages=["dmarclib"]
 )
